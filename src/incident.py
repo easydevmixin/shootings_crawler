992f6b40177d88b8b3dad6f7106e44342a98bd70
Ramon Maria Gallart <rgallart@ramagaes.com>
2018-04-07 12:46:37 -0700

    Additional data for incident added.

--- src/incident.py
+++ src/incident.py
@@ -15,6 +15,13 @@ class Incident:
         self.num_injured = kwargs.get('num_injured', 0)
         self.incident_link = kwargs.get('incident_link', '')
         self.sha256 = ''
+        self.lat = kwargs.get('lat', 0)
+        self.lon = kwargs.get('lon', 0)
+        self.participants = []  # list of dictionaries
+        self.characteristics = []  # list
+        self.notes = ''
+        self.guns_involved = []  # list of dictionaries
+        self.district = {}  # dictionary
 
     def __str__(self):
         return "{}/{}/{}, {}".format(self.year, self.month, self.day, self.incident_link)
@@ -131,6 +138,62 @@ class Incident:
             self.address, self.num_killed, self.num_injured, self.incident_link
         )
         self._sha256 = hashlib.sha256(st.encode(encoding='utf_8')).hexdigest()
+
+    @property
+    def lat(self):
+        return self._lat
+
+    @lat.setter
+    def lat(self, val):
+        self._lat = val
+
+    @property
+    def lon(self):
+        return self._lon
+
+    @lon.setter
+    def lon(self, val):
+        self._lon = val
+
+    @property
+    def participants(self):
+        return self._participants
+
+    @participants.setter
+    def participants(self, val):
+        self._participants = val
+
+    @property
+    def characteristics(self):
+        return self._characteristics
+
+    @characteristics.setter
+    def characteristics(self, val):
+        self._characteristics = val
+
+    @property
+    def notes(self):
+        return self._notes
+
+    @notes.setter
+    def notes(self, val):
+        self._notes = val
+
+    @property
+    def guns_involved(self):
+        return self._guns_involved
+
+    @guns_involved.setter
+    def guns_involved(self, val):
+        self._guns_involved = val
+
+    @property
+    def district(self):
+        return self._district
+
+    @district.setter
+    def district(self, val):
+        self._district = val
 
     def to_csv(self):
         return [self.sha256, self.year, self.month, self.day, self.state, self.city_or_county,
